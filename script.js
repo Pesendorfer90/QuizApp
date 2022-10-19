9127259f6e06c2e5fedd1a9786007eb8f99663a2
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2022-10-19 10:39:21 +0200

    clear

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = [
+    {
+        "question": "Werhat HTML erfunden",
+        "answer_1": "Robbie Williams";
+        "answer_2": "Lady Gaga";
+        "answer_3": "Tim Berner-Lee";
+        "answer_4": "Justin Bieber";
+        "right_answer": 3
+    }
+];
